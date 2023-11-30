8d98ea5a14fb430893bf77abde07f7147654fc75
DimitrisDris <dimitris.dris1@gmail.com>
2023-11-30 17:21:29 +0200

    template-v0.2

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var GROUND_SPRITE_WIDTH = 50
 var GROUND_SPRITE_HEIGHT = 50
 var numGroundSprites
 var GRAVITY = 0.3
-var JUMP = -5
+var JUMP = -10
 var isGameOver
 var score
 
@@ -14,8 +14,9 @@ function setup() {
     isGameOver = false
     score = 0
 
-    createCanvas(400, 300)
-    background(150, 200, 250)
+    createCanvas(800, 600)
+    background(255, 255, 255)
+
     groundSprites = new Group()
 
     numGroundSprites = width / GROUND_SPRITE_WIDTH + 1
@@ -31,6 +32,7 @@ function setup() {
 
     player = createSprite(100, height - 75, 50, 50)
     obstacleSprites = new Group()
+
 }
 
 function draw() {
@@ -43,25 +45,47 @@ function draw() {
     }else{
         background(150, 200, 250)
         player.velocity.y = player.velocity.y + GRAVITY
-    
+        camera.position.x = player.position.x + 5
+
         if (groundSprites.overlap(player)) {
             player.velocity.y = 0
             player.position.y = height - 50 - player.height / 2
         }
     
-        if (keyDown(UP_ARROW)) {
+        if (keyDown(UP_ARROW) && groundSprites.overlap(player)) {
             player.velocity.y = JUMP
         }
-        player.position.x = player.position.x + 5
-        camera.position.x = player.position.x + width / 4
+        if (keyDown(RIGHT_ARROW)) {
+            player.position.x = player.position.x + 5
+            camera.position.x = player.position.x + 5
+        }
+        if (keyDown(LEFT_ARROW)) {
+            player.position.x = player.position.x - 5
+            camera.position.x = player.position.x - 5
+        }
+
+        // player.position.x = player.position.x + 5
+        // camera.position.x = player.position.x + width / 4
+
         var firstGroundSprite = groundSprites[0]
         if (firstGroundSprite.position.x <= camera.position.x - (width / 2 + firstGroundSprite.width / 2)) {
             groundSprites.remove(firstGroundSprite)
             firstGroundSprite.position.x = firstGroundSprite.position.x + numGroundSprites * firstGroundSprite.width
             groundSprites.add(firstGroundSprite)
         }
-        if (random() > 0.95) {
-            var obstacle = createSprite(camera.position.x + width, random(0, height - 50 - 15), 30, 30)
+
+        var plat = createSprite(width - 100, height - 200, 200, 20)
+
+        if (plat.overlap(player)) {
+            player.position.y = height - 225
+        }
+
+
+
+
+        
+        if (random() > 0.98) {
+            var obstacle = createSprite(camera.position.x + width, height - 75, 50, 50)
             obstacleSprites.add(obstacle)
         }
     
@@ -72,7 +96,7 @@ function draw() {
     
         obstacleSprites.overlap(player, endGame)
         drawSprites()
-        score = score + 1
+        // score = score + 1
         textAlign(CENTER)
         text(score, camera.position.x, 20)
     }
